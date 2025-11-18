47a8b4fc8e8b7af08df42fd4db18b53a32eb8220
Pedro Feld <pedro.gael.feld@gmail.com>
2025-11-17 21:45:51 -0300

    feat: componentizando a funcionalidade de localizar CEPs

---
+++ src/components/CEP/index.tsx
@@ -0,0 +1,49 @@
+import axios from "axios";
+import { useState } from "react";
+
+interface IAddress {
+  logradouro: string
+  bairro: string
+  localidade: string
+  uf: string
+}
+
+function FindCEP() {
+  const [cep, steCep] = useState('');
+  const [address, setAddress] = useState<IAddress | null>(null);
+
+  async function fetchAddress(): Promise<void> {
+    try {
+      const response = await axios.get<IAddress>(
+        `https://viacep.com.br/ws/${cep}/json/`
+      );
+      setAddress(response.data);
+    } catch {
+      throw new Error('Erro ao buscar endereço')
+    }
+  }
+  
+  return (
+    <>
+      <h1>Pesquise o endereço pelo CEP</h1>
+      <input 
+        type="text" 
+        value={cep} 
+        onChange={(event) => steCep(event.target.value)} 
+        placeholder="Digita o CEP desejado"
+      />
+      <button onClick={fetchAddress}>Pesquisar</button>
+
+      {address && (
+        <div>
+          <p><span>Logradouro:</span> {address.logradouro}</p>
+          <p><span>Bairro:</span> {address.bairro}</p>
+          <p><span>Cidade:</span> {address.localidade}</p>
+          <p><span>UF:</span> {address.uf}</p>
+        </div>
+      )}
+    </>
+  )
+}
+
+export default FindCEP
